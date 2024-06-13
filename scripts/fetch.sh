c02a9c27cb1d49342ca5367ea793fd2ec8b88b56
Matthew Rodusek <7519129+bitwizeshift@users.noreply.github.com>
2024-06-12 20:54:05 -0500

    Add r4b structure definitions

--- scripts/fetch.sh
+++ scripts/fetch.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 root_dir="$(git rev-parse --show-toplevel)"
 readonly root_dir
 
@@ -215,6 +217,218 @@ r4=(
   visionprescription
 )
 
+r4b=(
+  # Abstract
+  element
+  backboneelement
+  resource
+  domainresource
+  # Primitive
+  base64binary
+  boolean
+  canonical
+  code
+  date
+  datetime
+  decimal
+  id
+  instant
+  integer
+  markdown
+  oid
+  positiveint
+  string
+  time
+  unsignedint
+  uri
+  url
+  uuid
+  data
+  address
+  age
+  annotation
+  attachment
+  backboneelement
+  codeableconcept
+  codeablereference
+  coding
+  contactdetail
+  contactpoint
+  contributor
+  count
+  datarequirement
+  distance
+  dosage
+  duration
+  element
+  elementdefinition
+  expression
+  extension
+  humanname
+  identifier
+  marketingstatus
+  meta
+  money
+  narrative
+  parameterdefinition
+  period
+  population
+  prodcharacteristic
+  productshelflife
+  quantity
+  range
+  ratio
+  ratiorange
+  reference
+  relatedartifact
+  sampleddata
+  signature
+  timing
+  triggerdefinition
+  usagecontext
+  # Resources
+  account
+  activitydefinition
+  administrableproductdefinition
+  adverseevent
+  allergyintolerance
+  appointment
+  appointmentresponse
+  auditevent
+  basic
+  binary
+  biologicallyderivedproduct
+  bodystructure
+  bundle
+  capabilitystatement
+  careplan
+  careteam
+  catalogentry
+  chargeitem
+  chargeitemdefinition
+  citation
+  claim
+  claimresponse
+  clinicalimpression
+  clinicalusedefinition
+  codesystem
+  communication
+  communicationrequest
+  compartmentdefinition
+  composition
+  conceptmap
+  condition
+  consent
+  contract
+  coverage
+  coverageeligibilityrequest
+  coverageeligibilityresponse
+  detectedissue
+  device
+  devicedefinition
+  devicemetric
+  devicerequest
+  deviceusestatement
+  diagnosticreport
+  documentmanifest
+  documentreference
+  encounter
+  endpoint
+  enrollmentrequest
+  enrollmentresponse
+  episodeofcare
+  eventdefinition
+  evidence
+  evidencereport
+  evidencevariable
+  examplescenario
+  explanationofbenefit
+  familymemberhistory
+  flag
+  goal
+  graphdefinition
+  group
+  guidanceresponse
+  healthcareservice
+  imagingstudy
+  immunization
+  immunizationevaluation
+  immunizationrecommendation
+  implementationguide
+  ingredient
+  insuranceplan
+  invoice
+  library
+  linkage
+  list
+  location
+  manufactureditemdefinition
+  measure
+  measurereport
+  media
+  medication
+  medicationadministration
+  medicationdispense
+  medicationknowledge
+  medicationrequest
+  medicationstatement
+  medicinalproductdefinition
+  messagedefinition
+  messageheader
+  molecularsequence
+  namingsystem
+  nutritionorder
+  nutritionproduct
+  observation
+  observationdefinition
+  operationdefinition
+  operationoutcome
+  organization
+  organizationaffiliation
+  packagedproductdefinition
+  patient
+  paymentnotice
+  paymentreconciliation
+  person
+  plandefinition
+  practitioner
+  practitionerrole
+  procedure
+  provenance
+  questionnaire
+  questionnaireresponse
+  regulatedauthorization
+  relatedperson
+  requestgroup
+  researchdefinition
+  researchelementdefinition
+  researchstudy
+  researchsubject
+  riskassessment
+  schedule
+  searchparameter
+  servicerequest
+  slot
+  specimen
+  specimendefinition
+  structuredefinition
+  structuremap
+  subscription
+  subscriptionstatus
+  subscriptiontopic
+  substance
+  substancedefinition
+  supplydelivery
+  supplyrequest
+  task
+  terminologycapabilities
+  testreport
+  testscript
+  valueset
+  verificationresult
+  visionprescription
+)
+
 r5=(
   # abstract
   element
@@ -259,7 +473,7 @@ r5=(
   contributor
   count
   datarequirement
-  datatype
+  # data type
   distance
   dosage
   duration
@@ -461,6 +675,11 @@ function download() {
 mkdir -p fhir/r4
 for r in "${r4[@]}"; do
   download "$r" "r4"
+done
+
+mkdir -p fhir/r4b
+for r in "${r4[@]}"; do
+  download "$r" "r4b"
 done
 
 mkdir -p fhir/r5
